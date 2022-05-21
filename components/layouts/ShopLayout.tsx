c1afa045aba7839ff4fbffa6dc74ad9b3856fb26
Jhoandris B <andybaronp@gmail.com>
2022-05-21 14:38:46 -0500

    Layout | NavBar

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,43 @@
+import Head from 'next/head'
+import { FC } from 'react'
+import Navbar from '../ui/Navbar'
+interface Props {
+  title: string
+  pageDescription: string
+  imageFullUrl?: string
+  children?: React.ReactNode | undefined
+}
+const ShopLayout: FC<Props> = ({
+  children,
+  title,
+  pageDescription,
+  imageFullUrl,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name='description' content={pageDescription} />
+        <meta name='og:title' content={title} />
+        <meta name='og:description' content={pageDescription} />
+        {imageFullUrl && <meta name='og:image' content={imageFullUrl} />}
+      </Head>
+      <nav>
+        <Navbar />
+      </nav>
+      {/* {Sidbar} */}
+      <main
+        style={{
+          margin: '80px auto',
+          maxWidth: '1440px',
+          padding: '0px 30px',
+        }}
+      >
+        {children}
+      </main>
+      <footer>{/* {custom footer} */}</footer>
+    </>
+  )
+}
+
+export default ShopLayout
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
+import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
+import ShopLayout from '../components/layouts/ShopLayout'
 
 const Home: NextPage = () => {
-  return <h1>HI</h1>
+  return (
+    <ShopLayout
+      title={'TesloShop | Home'}
+      pageDescription={'Encuentra lo mejor'}
+    >
+      <Typography variant='h1' component='h1'>
+        Tienda
+      </Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>
+        Todos los productos
+      </Typography>
+    </ShopLayout>
+  )
 }
 
 export default Home
