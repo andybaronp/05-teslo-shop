d5f9162b92827ae45fe47403bd7f3882564ee6f4
Jhoandris B <andybaronp@gmail.com>
2022-07-06 06:43:56 -0500

    Add validate jwt

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -17,3 +17,27 @@ export const signToken = (_id: string, email: string) => {
 
     )
 }
+
+
+
+export const isValidToken = (token: string): Promise<string> => {
+    if (!process.env.JWT_SECRET_SEDD) {
+        throw new Error("Verifique las variables de entorno")
+    }
+
+    return new Promise((resolve, reject) => {
+
+        try {
+            jwt.verify(token, process.env.JWT_SECRET_SEDD || '', (err, payload) => {
+                if (err) return reject('JWT no valido')
+                const { _id } = payload as { _id: string }
+
+                resolve(_id)
+            })
+
+        } catch (error) {
+            reject('JWT no valido')
+        }
+
+    })
+}
